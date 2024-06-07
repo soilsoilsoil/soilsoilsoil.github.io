5ed2bc5c463c36b7905ba192bf83ec65b45228d9
soilsoilsoil <39055751+soilsoilsoil@users.noreply.github.com>
2024-06-07 15:55:54 +0800

    firstPush
    firstPush

---
+++ src/settings.b17c7.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scenes/TelegramTest.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "d36c4",
+        main: "78f45"
+    }
+};
